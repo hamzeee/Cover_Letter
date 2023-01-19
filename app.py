f330ce4f50964eeedb6d89e055be5e82dfad1796
Hamza Arif Raza <hamzaarif.95@gmail.com>
2023-01-19 22:01:31 +0000

    changed the prompt

--- app.py
+++ app.py
@@ -23,13 +23,4 @@ def index():
 
 
 def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+    return "Suggest three names for an cow that is a superhero"
