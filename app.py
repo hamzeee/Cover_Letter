7a233e7f8713fb79156eff7a25cb521a3960713c
Hamza Arif Raza <hamzaarif.95@gmail.com>
2023-01-22 17:14:06 +0000

    first iteration

--- app.py
+++ app.py
@@ -11,9 +11,14 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def index():
     if request.method == "POST":
         name = request.form["name"]
+        degree = request.form["degree"]
+        university = request.form["university"]
+        job = request.form["job"]
+        company = request.form["company"]
+
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(name),
+            prompt=generate_prompt(name,degree,university,job,company),
             temperature=0.6,
             max_tokens=300,
         )
@@ -23,8 +28,7 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(name):
-    return """Generate a cover letter for a mechanical engineer {} in the
-    automotive industry who is looking to apply to the company Toyota for the
-    position of Manufacturing Specialist
-    """.format(name)
+def generate_prompt(name,degree,university,job,company):
+    return """Generate a cover letter for {} who is a {} 
+    graduate from {} applying for the poisition of {} in the company {} 
+    """.format(name,degree,university,job,company)
